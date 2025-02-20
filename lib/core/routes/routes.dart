41f3f37f010d62011f0a38bc156628fb3f09acee
Alaanaser714 <alaanaser577@gmail.com>
2025-02-20 16:22:58 +0200

    finshed logic about splash

--- lib/core/routes/routes.dart
+++ lib/core/routes/routes.dart
@@ -1,4 +1,5 @@
 import 'package:e_commerce/core/routes/app_routes.dart';
+import 'package:e_commerce/features/on_boarding/presentation/views/on_boarding_screen.dart';
 import 'package:e_commerce/features/splash/presentation/view/splash_screen.dart';
 import 'package:go_router/go_router.dart';
 
@@ -9,6 +10,12 @@ abstract class Routes {
         path: AppRoutes.splash,
         builder: (context, state) {
           return const SplashScreen();
+        },
+      ),
+      GoRoute(
+        path: AppRoutes.onBoarding,
+        builder: (context, state) {
+          return const OnBoardingScreen();
         },
       ),
     ],
--- lib/features/splash/presentation/view/splash_screen.dart
+++ lib/features/splash/presentation/view/splash_screen.dart
@@ -1,9 +1,24 @@
+import 'package:e_commerce/core/routes/app_routes.dart';
 import 'package:e_commerce/core/utils/app_assets.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:svg_flutter/svg.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Future.delayed(Duration(seconds: 3), () {
+      GoRouter.of(context).push(AppRoutes.onBoarding);
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
