de5c0f958d476ab91dbacf9c3348fec8d865d68d
Alaanaser714 <alaanaser577@gmail.com>
2025-02-21 16:47:46 +0200

    add app colors

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color primaryColor = Color(0xff1B5E37);
+  static const Color orangeColor = Color(0xffF4A91F);
+  static const Color blackColor = Color(0xff0C0D0D);
+  static const Color secondColor = Color(0xff2D9F5D);
+}
