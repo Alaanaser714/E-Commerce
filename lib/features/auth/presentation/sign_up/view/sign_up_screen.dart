b4cc25d85004c9947f0bc4522f5fa9a221075775
Alaanaser714 <alaanaser577@gmail.com>
2025-02-24 20:59:26 +0200

    intgrate sign up cubit

--- lib/features/auth/presentation/sign_up/view/sign_up_screen.dart
+++ lib/features/auth/presentation/sign_up/view/sign_up_screen.dart
@@ -1,16 +1,10 @@
-import 'package:e_commerce/core/routes/app_routes.dart';
 import 'package:e_commerce/core/services/get_it_services.dart';
-import 'package:e_commerce/core/widgets/custom_app_bar.dart';
-import 'package:e_commerce/core/widgets/custom_button.dart';
-import 'package:e_commerce/core/widgets/custom_text_form_field.dart';
-import 'package:e_commerce/core/widgets/custom_text_rich.dart';
 import 'package:e_commerce/features/auth/domain/repo/auth_repo.dart';
 import 'package:e_commerce/features/auth/presentation/cubits/signup_cubits/signup_cubit.dart';
+import 'package:e_commerce/features/auth/presentation/cubits/signup_cubits/signup_state.dart';
+import 'package:e_commerce/features/auth/presentation/sign_up/widgets/sign_up_screen_body.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:go_router/go_router.dart';
-
-import '../widgets/checked_box_widget.dart';
 
 class SignUpScreen extends StatelessWidget {
   const SignUpScreen({super.key});
@@ -21,49 +15,17 @@ class SignUpScreen extends StatelessWidget {
       create: (context) => SignupCubit(
         getIt<AuthRepo>(),
       ),
-      child: Scaffold(
-        appBar: CustomAppBar(title: "حساب جديد"),
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                CustomTextFormField(
-                  keyboardType: TextInputType.name,
-                  hintText: "الاسم كامل",
-                ),
-                SizedBox(
-                  height: 16,
-                ),
-                CustomTextFormField(
-                  keyboardType: TextInputType.emailAddress,
-                  hintText: 'البريد الإلكتروني',
-                ),
-                SizedBox(
-                  height: 16,
-                ),
-                CustomTextFormField(hintText: "كلمة المرور", isPassword: true),
-                SizedBox(
-                  height: 10,
-                ),
-                CheckedBoxWidget(),
-                SizedBox(
-                  height: 30,
-                ),
-                CustomButton(text: "إنشاء حساب جديد"),
-                SizedBox(
-                  height: 30,
-                ),
-                CustomTextRich(
-                    onTap: () {
-                      GoRouter.of(context).push(AppRoutes.login);
-                    },
-                    text: " تمتلك حساب بالفعل؟",
-                    secondtext: "  تسجيل دخول")
-              ],
-            ),
-          ),
-        ),
+      child: Builder(
+        builder: (context) {
+          return BlocConsumer<SignupCubit, SignupState>(
+            listener: (context, state) {
+              
+            },
+            builder: (context, state) {
+              return SignUpScreenBody();
+            },
+          );
+        },
       ),
     );
   }
