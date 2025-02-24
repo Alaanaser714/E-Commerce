c52a7c3f5210925ad7a781cb47623568582a285d
Alaanaser714 <alaanaser577@gmail.com>
2025-02-24 22:26:01 +0200

    create login cubit

---
+++ lib/features/auth/presentation/cubits/login_cubit/login_cubit.dart
@@ -0,0 +1,29 @@
+import 'package:bloc/bloc.dart';
+import 'package:e_commerce/features/auth/domain/entities/user_entity.dart';
+import 'package:e_commerce/features/auth/domain/repo/auth_repo.dart';
+import 'package:meta/meta.dart';
+
+part 'login_state.dart';
+
+class LoginCubit extends Cubit<LoginState> {
+  LoginCubit(this.authRepo) : super(LoginInitial());
+
+  final AuthRepo authRepo;
+  Future<void> loginWithEmailAndPassword(
+      String email, String password, String name) async {
+    emit(LoginLoading());
+    final result = await authRepo.loginUserWithEmailAndPassword(
+      email,
+      password,
+      name,
+    );
+    result.fold(
+      (failure) => emit(
+        LoginFailure(message: failure.message),
+      ),
+      (userEntity) => emit(
+        LoginSuccess(userEntity: userEntity),
+      ),
+    );
+  }
+}
