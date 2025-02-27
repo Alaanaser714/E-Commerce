3160d63fb2fd59cebb3dff1cf65fd5098dd026b5
Alaanaser714 <alaanaser577@gmail.com>
2025-02-27 09:57:41 +0200

    remove apple method

--- lib/features/auth/presentation/login/widgets/login_screen_body.dart
+++ lib/features/auth/presentation/login/widgets/login_screen_body.dart
@@ -117,11 +117,6 @@ class _LoginScreenBodyState extends State<LoginScreenBody> {
                   text: 'تسجيل بواسطة جوجل',
                 ),
                 CustomSocailWidgets(
-                  image: Assets.Apple,
-                  text: "تسجيل بواسطة أبل",
-                  onTap: () {},
-                ),
-                CustomSocailWidgets(
                   image: Assets.Face,
                   text: "تسجيل بواسطة فيسبوك",
                   onTap: () {
